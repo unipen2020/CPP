f34d3708e71c11241f8624d0b05fa1eb3b123d57
Sziva Barnabas <szivabarnabas@gmail.com>
2024-02-23 08:59:41 +0100

    1. commit: main.cpp és mododfoku.h létrehozása

---
+++ Week_1/masodfoku.h
@@ -0,0 +1,16 @@
+class Masodfoku
+{
+   private:
+      int  a ;
+      int  b ;
+      int  c ;
+   public:
+      Masodfoku(int a, int b, int c)   ;
+      Masodfoku(const Masodfoku& m)    ; 
+   private:
+      int D()                          ;
+      int megoldasok()                 ;
+      double X_1()                     ;
+      double X_2()                     ;
+}
+;
